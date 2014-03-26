d066d94e01f483665f403e9a085cea611e1e601b
cloneko <cloneko@gmail.com>
2014-03-26 19:29:22 +0900

    Edit for Cinnamon

--- lmde/attributes/default.rb
+++ lmde/attributes/default.rb
@@ -1,7 +1,7 @@
 default['lmde']['iso']['cinnamon'] = 'http://172.16.40.3/isos/linux/linuxmint-201403-cinnamon-dvd-64bit.iso'
 default['lmde']['iso']['mate'] = 'http://172.16.40.3/isos/linux/linuxmint-201403-mate-dvd-64bit.iso'
 
-default['lmde']['iso']['localmate'] = 'linuxmint-201403-mate-dvd-64bit.iso'
+default['lmde']['iso']['localmate'] = 'linuxmint-201403-cinnamon-dvd-64bit.iso'
 
 default['workdir'] = '/tmp'
 default['tftpbootdir'] = '/var/lib/tftpboot'
